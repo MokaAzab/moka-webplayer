3ba42ca6e95cfab61e1d553f9fabb52be920ab7a
MokaAzab <azab94@gmail.com>
2025-10-22 21:20:34 +0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,20 @@
+export default function handler(req, res) {
+  const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+  const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+  const scopes = [
+    "streaming",
+    "user-read-email",
+    "user-read-private",
+    "user-modify-playback-state",
+    "user-read-playback-state"
+  ];
 
+  const authUrl =
+    "https://accounts.spotify.com/authorize" +
+    "?response_type=token" +
+    "&client_id=" + CLIENT_ID +
+    "&scope=" + encodeURIComponent(scopes.join(" ")) +
+    "&redirect_uri=" + encodeURIComponent(REDIRECT_URI);
+
+  res.redirect(authUrl);
+}
